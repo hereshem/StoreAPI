7ca7435762776ef9747861d45d48abf77f58970c
Hem Shrestha <hereshem@gmail.com>
2017-02-11 00:31:45 +0545

    database connection optimized

--- connection.php
+++ connection.php
@@ -1,6 +1,5 @@
 <?php
-$connect=mysqli_connect('localhost','root','','hem_store');
-if(!$connect){
-	die("Connection error ".mysqli_connect_error());
-}
+	// ( host, user, password, database )
+	$connect=mysqli_connect('localhost','root','','hem_store') or die("Connection error ".mysqli_connect_error());
+
 ?>
